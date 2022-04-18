3d3db43f9a4cdf9d1115a8c6ba44d14628b4c3ef
Oduo24 <geraldoduo@gmail.com>
2022-04-18 22:03:36 +0000

    added missing prototypes

--- main.h
+++ main.h
@@ -20,5 +20,10 @@ typedef struct print
 } print_t;
 
 int _printf(const char *format, ...);
+
+int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
+int ev_print_func(const char *s, int index);
+unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
+int print_buf(char *buf, unsigned int nbuf);
 #endif
 
